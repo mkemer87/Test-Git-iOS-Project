9e9985c0831db171da66bbc2565d7b27185552f9
Michael Merchant <mmerchant@michaels-mbp.home>
2014-04-15 19:19:20 -0400

    add a test class

---
+++ Git Test/MMTest.h
@@ -0,0 +1,13 @@
+//
+//  MMTest.h
+//  Git Test
+//
+//  Created by Michael Merchant on 4/15/14.
+//  Copyright (c) 2014 Michael Merchant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMTest : NSObject
+
+@end
---
+++ Git Test/MMTest.m
@@ -0,0 +1,13 @@
+//
+//  MMTest.m
+//  Git Test
+//
+//  Created by Michael Merchant on 4/15/14.
+//  Copyright (c) 2014 Michael Merchant. All rights reserved.
+//
+
+#import "MMTest.h"
+
+@implementation MMTest
+
+@end
